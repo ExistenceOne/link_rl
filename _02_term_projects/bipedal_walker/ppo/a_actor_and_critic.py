287401018a4c27b2ba4cd41f8c0d24b2f24da0f3
ExistenceOne <121415293+ExistenceOne@users.noreply.github.com>
2026-05-31 13:23:13 +0900

    modify: std max=0.5->0.8, std init=0.37->0.5, entropy_beta=0.01->0.02

--- _02_term_projects/bipedal_walker/ppo/a_actor_and_critic.py
+++ _02_term_projects/bipedal_walker/ppo/a_actor_and_critic.py
@@ -22,8 +22,8 @@ class Actor(nn.Module):
         self.fc2 = nn.Linear(128, 128)
         self.mu = nn.Linear(128, n_actions)
 
-        # ln_e(x) = -1.0 --> x = e^-1.0 = 0.37
-        log_std_param = nn.Parameter(torch.full((n_actions,), -1.0))
+        # ln(x) = -0.693 --> x = e^-0.693 = 0.5
+        log_std_param = nn.Parameter(torch.full((n_actions,), -0.693))
         self.register_parameter("log_std", log_std_param)
         self.to(DEVICE)
 

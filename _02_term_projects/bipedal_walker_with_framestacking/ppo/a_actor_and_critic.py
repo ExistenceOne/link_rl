448de150789946ac921013eda7028cfb0ee4671b
ExistenceOne <121415293+ExistenceOne@users.noreply.github.com>
2026-06-11 17:51:30 +0900

    PPO reduce entropy

--- _02_term_projects/bipedal_walker_with_framestacking/ppo/a_actor_and_critic.py
+++ _02_term_projects/bipedal_walker_with_framestacking/ppo/a_actor_and_critic.py
@@ -22,8 +22,8 @@ class Actor(nn.Module):
         self.fc2 = nn.Linear(512, 512)
         self.mu = nn.Linear(512, n_actions)
 
-        # ln(x) = -0.693 --> x = e^-0.693 = 0.5
-        log_std_param = nn.Parameter(torch.full((n_actions,), -0.693))
+        # ln(x) = -0.224 --> x = e^-0.224 = 0.799
+        log_std_param = nn.Parameter(torch.full((n_actions,), -0.224))
         self.register_parameter("log_std", log_std_param)
         self.to(DEVICE)
 
